9e3eb3ac919be9ae590c627782db0883e7a58856
Manuch Sadri <colonquebang@hotmail.com>
2023-03-23 06:12:47 +0000

    added sum_series() with tests

--- series/series.py
+++ series/series.py
@@ -1,19 +1,17 @@
-#def main():
-#    fibonacci(7)
-
-# find the nth term of the Fibonacci sequence iteratively
-def fibonacci(n):
+# find the nth term of the Fibonacci sequence iteratively, uses optional params for non-Fibonacci series
+def fibonacci(n, x = 0, y = 1):
     """
-    :param n:
+    :param n: required, function calculates this nth position of numerical series
+    :param x: optional; 1st number of series to calculate; defaults to 0 for Fibonacci
+    :param y: optional, 2nd number of series to calculate; defaults to 1 for Fibonacci
     :return: nth term of Fibonacci sequence
     """
-    if isinstance(n, int) and n >= 0:
-        fib_list = [0, 1]
+    if isinstance(n, int) and isinstance(x, int) and isinstance(y, int) and n >= 0:
+        fib_list = [x, y]
         i = 0
         while i < (n - 1):
             fib_list.append(fib_list[i] + fib_list[i + 1])
             i += 1
-        #print(fib_list[n])
         return fib_list[n]
     else:
         return "please enter an integer >= 0"
@@ -35,7 +33,11 @@ def lucas(n):
         return lucas(n - 1) + lucas(n - 2)
 
 # find the nth term of a user specified series with 1 required and 2 optional parameters
-def sum_series():
-    pass
-
-#main()
+def sum_series(n, x = 0, y = 1):
+    """
+    :param n: required, function calculates this nth position of numerical series
+    :param x: optional; 1st number of series to calculate; defaults to 0 for Fibonacci
+    :param y: optional, 2nd number of series to calculate; defaults to 1 for Fibonacci
+    :return: nth term of calculated series
+    """
+    return fibonacci(n, x, y)
